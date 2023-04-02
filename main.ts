f69d1ffc0bd1ed426d0c448977ac41f6a78afe71
ee945 <ee945@126.com>
2023-04-02 12:10:28 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (796 - 98))
+    basic.clearScreen()
+    basic.pause(100)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
